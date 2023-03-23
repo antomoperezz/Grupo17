f342f4faaa09e02cc99935bb68c898096b83dd6e
antomoperezz <antomo08@ucm.es>
2023-03-24 00:08:07 +0100

    Primeros cambios en la rama

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Hola, vamos a intentar crear un error de tipo 3");
+            int i = 0, j = 2;
+            int h = i + 2 * j;
+            Console.WriteLine("El resultado es " + h);
+           
+
         }
     }
 }
--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Habituarnos al ciclo de trabajo con Git
 Vamos a generar un conflicto
 Estoy generando un conflicto al realizar cambios al mismo tiempo que Antonio
 Vamos a por el conflicto de tipo 3
-Empezamos por el apartado 6
 
